882ace2119ecb8b1f7e4c1fdb2243a8e937e71b2
Your Name <your.email@example.com>
2025-06-06 21:42:14 +0300

    createsbscope file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+// Declare global variable
+var customerName = 'bob';
+
+// Uppercase the global customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Declare a global variable from inside a function
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // No `var`, `let`, or `const` — becomes global
+}
+
+// Overwrite the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'Karen';
+
+// Try to change the constant (will throw error if executed)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; 
+}
